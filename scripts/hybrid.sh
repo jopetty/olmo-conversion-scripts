55ccaa864bb957131f546652da3e11b652449449
Jackson Petty <jacksonp@allenai.org>
2026-06-02 11:23:53 -0700

    deps

---
+++ scripts/hybrid.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+TRUST_REMOTE_CODE=True uv run python yashas_hybrid.py \
+    --input_dir /weka/oe-training-default/ai2-llm/model-ladders/olmo3-hybrid-gdn/1B/step0 \
+    --output_dir /weka/oe-training-default/ai2-llm/checkpoints/jacksonp/olmo3-hybrid-gdn/1B/step0 \
+    --tokenizer allenai/Olmo-Hybrid-7B
